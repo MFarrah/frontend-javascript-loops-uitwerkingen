d3c7b48aebd5c8b1b3f998354865b319177da102
MFarr <micfar85@gmail.com>
2023-12-30 12:58:29 +0100

    loops uitgewerkt

--- loops.js
+++ loops.js
@@ -11,8 +11,9 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
-
-
+for (let i = 0; i < 5; i++) {
+    console.log("*".repeat(i + 1));
+}
 
 
 // ==========================================
@@ -25,8 +26,13 @@
 // klaar!
 // ==========================================
 
-
-
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log("loop...");
+    } else {
+        console.log("klaar!");
+    }
+}
 
 
 // ==========================================
@@ -45,8 +51,17 @@
 // >> 9
 // ==========================================
 
-
-
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i);
+    }
+    if (i >= 3 && i < 6) {
+        console.log(">" + i);
+    }
+    if (i >= 6) {
+        console.log(">>" + i);
+    }
+}
 
 
 // ==========================================
@@ -87,5 +102,17 @@
 // etc.
 // ==========================================
 
-
+for (let i = 1; i < 101; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }else {
+        console.log(i)
+    }
+}
 
